42a6410049977478879d14e5c54dc3eaf0d5a6b3
Hailah Alghannam <hailahalghannam@Hailahs-MacBook-Pro.local>
2026-04-23 17:10:56 +0300

    video call with camera switch

--- lib/view/video_call_view.dart
+++ lib/view/video_call_view.dart
@@ -17,21 +17,30 @@ class VideoCallView extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: ZegoUIKitPrebuiltCall(
-        appID: 1561145060, // خلي أرقامك زي ما هي
+        appID: 1561145060,
         appSign: "2d4b9e89e5e3d2dc4625071e52091562ae81fe354f86a6073c9e9c1afcecda3b",
         userID: userID,
         userName: userName,
         callID: callID,
 
-        // التعديل هنا: أضفنا زر مشاركة الشاشة مع الأزرار الأساسية
         config: ZegoUIKitPrebuiltCallConfig.groupVideoCall()
+          ..bottomMenuBar.maxCount = 6
           ..bottomMenuBar.buttons = [
+            ZegoCallMenuBarButtonName.hangUpButton,
             ZegoCallMenuBarButtonName.toggleCameraButton,
             ZegoCallMenuBarButtonName.toggleMicrophoneButton,
+            ZegoCallMenuBarButtonName.switchCameraButton,
             ZegoCallMenuBarButtonName.switchAudioOutputButton,
-            ZegoCallMenuBarButtonName.toggleScreenSharingButton, // 👈 زر مشاركة الشاشة
-            ZegoCallMenuBarButtonName.hangUpButton,
+            ZegoCallMenuBarButtonName.toggleScreenSharingButton,
           ],
+
+        events: ZegoUIKitPrebuiltCallEvents(
+          onCallEnd: (event, defaultAction) {
+            if (event.reason == ZegoUIKitCallEndReason.localHangUp) {
+              Navigator.of(context, rootNavigator: true).pop();
+            }
+          },
+        ),
       ),
     );
   }
